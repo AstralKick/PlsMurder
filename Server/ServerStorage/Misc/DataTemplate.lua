82ad13c98ad7b2db18438c2ffe57a3e6336d1b93
AstralKick <99090881+AstralKick@users.noreply.github.com>
2023-08-11 00:52:18 +0100

    Added in proxy, retrieves the gamepasses and TShirts now

---
+++ Server/ServerStorage/Misc/DataTemplate.lua
@@ -0,0 +1,3 @@
+return {
+    Currency = 0,
+}
---
+++ Server/ServerStorage/Util/ApiRequests.lua
@@ -0,0 +1,98 @@
+local HttpService = game:GetService("HttpService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Packages = ReplicatedStorage.Packages
+local Promise = require(Packages.Promise)
+local TblUtil = require(Packages.TblUtil)
+
+
+--@param This is the subcategories for the catalog.
+local SubCategories = {"3"}
+
+--local Url = "https://catalog.roproxy.com/v1/search/items/details?Category=3&Subcategory=".. SubCategories[i].. "&Sort=4&Limit=30&CreatorName=%s&cursor=%s"
+
+local Urls = {
+    Catalog = "https://catalog.roproxy.com/v1/search/items/details?Category=3&Subcategory=%s&Sort=4&Limit=30&CreatorName=%s&cursor=%s", -- Urls.Catalog:format(subcategory, username, cursor)
+    Gamepasses = "https://www.roproxy.com/users/inventory/list-json?assetTypeId=34&cursor=&itemsPerPage=100&pageNumber=%s&userId=%s",
+    Likes = "https://games.roproxy.com/v1/games/votes?universeIds=%s"
+}
+
+-- Game Id
+local GameId = 891852901
+
+local ApiRequests = {}
+
+ApiRequests.__index = ApiRequests
+
+local function MakeHttpRequest(Url: string)
+    return Promise.new(function(Resolve, Reject)
+        local Request = HttpService:GetAsync(Url, true)
+        if not Request then
+            Reject()
+        end
+        local RequestLength = Request:len()
+        Request = HttpService:JSONDecode(Request)
+        Resolve(Request, RequestLength)
+    end)
+end
+
+
+function ApiRequests.new(Player: Player)
+    local self = setmetatable({}, ApiRequests)
+
+    self.Player = Player
+    self.Gamepasses = {}
+    self.TShirts = {}
+
+    return self
+end
+
+function ApiRequests:GetItems()
+    local Player = self.Player
+    for _,Category in ipairs (SubCategories) do
+        local Cursor = ""
+        local RequestUrl = Urls.Catalog:format(Category, Player.Name, Cursor)
+
+        MakeHttpRequest(RequestUrl):andThen(function(Result)
+            self.TShirts = TblUtil.Copy(Result.data, true)
+        end):catch(function()
+            warn(Player.Name, " HAS NO ITEMS ON CATEGORY ", Category)
+        end):await()
+    end
+
+
+    local function RecursiveGamepassCollection(Player: Player)
+        local GamepassUrl = Urls.Gamepasses:format(tostring(self.GamepassUrlLength), Player.UserId)
+        self.PageNumber = self.PageNumber or 1 
+        self.GamepassUrlLength = self.GamepassUrlLength or math.huge
+
+        MakeHttpRequest(GamepassUrl):andThen(function(Result: {}, RequestLength: number)
+            for i,gamepass in ipairs (Result.Data.Items) do
+                if gamepass.Creator.Id ~= Player.UserId then continue end
+                self.Gamepasses[#self.Gamepasses+1] = TblUtil.Copy(gamepass, true)
+            end
+
+            if RequestLength ~= self.GamepassUrlLength then
+                self.GamepassUrlLength = RequestLength
+                self.PageNumber += 1
+                RecursiveGamepassCollection(Player)
+            end
+
+        end):catch(function()
+            warn(Player.Name, " HAS NO GAMEPASSES")
+        end):await()
+    end
+
+    RecursiveGamepassCollection(Player)
+end
+
+function ApiRequests:GetLikes()
+    local UrlRequest = Urls.Likes:format(tostring(GameId))
+    MakeHttpRequest(UrlRequest):andThen(function(Result)
+        print(Result)
+    end):catch(function()
+        warn("Unable to fetch like data")
+    end)
+end
+
+return ApiRequests
